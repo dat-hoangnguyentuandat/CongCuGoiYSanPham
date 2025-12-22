ba8af54c54ed282ed613e51baeb41eb17db9dfb3
tuandat <hoangdatlnbp@gmail.com>
2025-12-22 09:00:00 +0700

    Complete admin area setup

---
+++ Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CongCuGoiYSanPham.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Index", "Dashboard");
+        }
+    }
+}
