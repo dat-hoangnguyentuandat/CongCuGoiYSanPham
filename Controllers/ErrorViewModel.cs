b9f4eb0a6937617a660c930d7f8b44f279715060
tuandat <hoangdatlnbp@gmail.com>
2025-12-24 09:00:00 +0700

    Complete AI recommendation system and final project setup

---
+++ Controllers/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CongCuGoiYSanPham.Controllers
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+        
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
